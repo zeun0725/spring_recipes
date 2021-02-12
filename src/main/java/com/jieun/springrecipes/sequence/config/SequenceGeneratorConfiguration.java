6e558d8079c2c334841892a12a8a30a98791b74a
parkjieun <wldmsdl3187@naver.com>
2021-02-12 17:16:06 +0900

    spring framework setting and code modified

--- src/main/java/com/jieun/springrecipes/sequence/config/SequenceGeneratorConfiguration.java
+++ src/main/java/com/jieun/springrecipes/sequence/config/SequenceGeneratorConfiguration.java
@@ -1,4 +1,20 @@
 package com.jieun.springrecipes.sequence.config;
 
+import com.jieun.springrecipes.sequence.SequenceGenerator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class SequenceGeneratorConfiguration {
+
+    @Bean
+    public SequenceGenerator sequenceGenerator(){
+        SequenceGenerator seqgen = new SequenceGenerator();
+        seqgen.setPrefix("30");
+        seqgen.setSuffix("A");
+        seqgen.setInitial(10000);
+        return seqgen;
+    }
+
+
 }
